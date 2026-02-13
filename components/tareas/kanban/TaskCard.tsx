8a2b1caba2c8a78ea2b5b3574a3c58b821a654e3
Landa5 <info@selsa.es>
2026-02-13 17:51:16 +0100

    Fix Vercel build error: Rename dropdown-menu.tsx to DropdownMenu.tsx

--- components/tareas/kanban/TaskCard.tsx
+++ components/tareas/kanban/TaskCard.tsx
@@ -23,7 +23,7 @@ import {
     DropdownMenuLabel,
     DropdownMenuSeparator,
     DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+} from "@/components/ui/DropdownMenu";
 
 interface TaskCardProps {
     task: any;
